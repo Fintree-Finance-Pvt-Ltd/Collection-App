21ff3ae48dd34b8a449a38545139ff202c2a086b
Harish-lodh <harishlodh444@gmail.com>
2026-04-14 13:52:23 +0530

    fix webhook

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: '15mb' }));
+
+app.use(express.urlencoded({extended: true }));
 app.use("/uploads", express.static("uploads"));
 
 
