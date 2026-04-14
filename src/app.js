c43aed9421ced5115bd4d29b660c4e8201187c51
Harish-lodh <harishlodh444@gmail.com>
2026-04-14 13:41:06 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ import paymentsgateway from './routes/digitalCollect.js';
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '1mb' }));
+app.use(express.json({ limit: '15mb' }));
 app.use("/uploads", express.static("uploads"));
 
 
